36059bee5a65ff76d850dfa941498f6a888a55c3
Jeremy Shank <jpshank2@gmail.com>
2022-12-07 10:25:41 -0600

    attempting to connect streamlit to snowflake

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import snowflake.connector
 
 st.write("# Hello, build! :wave:")
+
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM mytable;")
+
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
