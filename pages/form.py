fdfeaccc590b4b6ef4a08bc9a956b8692788ae1c
Jeremy Shank <jpshank2@gmail.com>
2022-12-07 14:12:31 -0600

    creating input form

--- pages/form.py
+++ pages/form.py
@@ -1,15 +1,33 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
-def displayInfo(fname, lname, state):
-    st.write(f"Hello, {fname} {lname} from {state}")
+import math, datetime
 
 st.write("# Hello, form! :wave:")
 
-first_name = st.text_input('First Name')
-last_name = st.text_input('Last Name')
+today = datetime.datetime.now()
 
-state = st.selectbox("State", ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'])
+with st.form('greet_form', clear_on_submit=True):
+    first_name = st.text_input('First Name')
+    last_name = st.text_input('Last Name')
+    state = st.selectbox("Home State", ['', 'AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'])
+    birthday = st.date_input('Birth Date')
 
-clicked = st.button("Greetings!", on_click=displayInfo, args=(first_name, last_name, state))
+    age = math.floor((today - birthday) / pd.timedelta(days=365))
+
+    submitted = st.form_submit_button("Greet!")
+    if submitted:
+        if first_name == "" or first_name == None:
+            st.warning("Please input a first name")
+            st.stop()
+        if last_name == "" or last_name == None:
+            st.warning("Please input a last name")
+            st.stop()
+        elif state == '':
+            st.warning("Please select a state")
+            st.stop()
+        elif birthday == '' or birthday == None:
+            st.warning("Please select a birth date")
+            st.stop()
+        else:
+            st.write(f"Hello, {first_name} {last_name} from {state}. You are {age} years old")
