af35c6231a9cbb7a47b7db17f8a63a9cd95d93a6
Jeremy Shank <jpshank2@gmail.com>
2022-12-09 14:28:00 -0600

    adding cronjob to test emailing

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,39 @@
 import streamlit as st
+from crontab import CronTab
+import smtplib, datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def emailMe():
+    try:
+        msg = MIMEMultipart('alternative')
+        msg['From'] = st.secrets['zeal_address']
+        msg['To'] = 'jshank@abacustechnologies.com'
+        msg['Subject'] = "Hello from streamlit"
+
+        body_html = f"<p>Hello from streamlit. It is currently {datetime.datetime.now().strftime('%I:%M %p')}</p>"
+        mimeBody = MIMEText(body_html, 'html')
+        msg.attach(mimeBody)
+
+        mail = smtplib.SMTP("smtp.outlook.office365.com", 587, timeout=20)
+        mail.starttls()
+        
+        recipient = ['jshank@abacustechnologies.com']
+        
+        mail.login(st.secrets['zeal_address'], st.secrets['zeal_pass'])
+        mail.sendmail(st.secrets['zeal_address'], recipient, msg.as_string())
+        mail.quit()
+    except Exception as e:
+        st.error(e)
 
 st.write("# Hello, home! :wave:")
 st.snow()
 
 st.write("This is the Abacus Technologies test site for creating streamlit apps for Business Intelligence. If you are interested in our services, please [email us](mailto:bizintel@abacustechnologies.com?subject=Streamlit).")
+
+cron = CronTab(user=None)
+
+job = cron.new(command=emailMe)
+job.hour.every(12)
+
+cron.write()
