28d202818b29e4a2450d7aed6d6c8e9a019fa9dd
Jeremy Shank <jpshank2@gmail.com>
2023-02-22 09:41:00 -0600

    removed fetch_pandas_all

--- pages/wip.py
+++ pages/wip.py
@@ -17,7 +17,7 @@ conn = init_connection()
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
-        return cur.fetch_pandas_all()
+        return cur.fetchall()
 
 try:
     rows = run_query("SELECT top 10 * from TRANS_AR AR INNER JOIN DIM_CLIENT_MASTER C ON C.ContIndex = AR.ContIndex;")
