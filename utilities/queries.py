2ba0713ae2a9813753e02c51c07d61d51c8d08e9
Jeremy Shank <jpshank2@gmail.com>
2023-02-24 15:21:29 -0600

    try catch for run_query

--- utilities/queries.py
+++ utilities/queries.py
@@ -4,21 +4,25 @@ from math import ceil
 
 @cache_data(ttl=3600)
 def run_query(query, _conn, rows):
-    iterations = ceil(rows / 10000)
-    finalList = list()
-    for i in range(iterations):
-        offset = i * 10000
-        query += f' LIMIT 10000 OFFSET {offset};'
-        print(query)
-        with _conn.cursor() as cur:
-            cur.execute(query)
-            rows = cur.fetchall()
-            columns = [column[0] for column in cur.description]
-            results = list()
-            for row in rows:
-                results.append(dict(zip(columns, row)))
-            
-            finalList.append(results)
+    try:
+        iterations = ceil(rows / 10000)
+        finalList = list()
+        for i in range(iterations):
+            offset = i * 10000
+            query += f'\nLIMIT 10000 OFFSET {offset};'
+            print(query)
+            with _conn.cursor() as cur:
+                cur.execute(query)
+                rows = cur.fetchall()
+                columns = [column[0] for column in cur.description]
+                results = list()
+                for row in rows:
+                    results.append(dict(zip(columns, row)))
+                
+                finalList.append(results)
+    except Exception as e:
+        query += '\nLIMIT 10000 OFFSET 0;'
+        return {'query': query, 'e': e}
             
     return DataFrame(finalList)
     
