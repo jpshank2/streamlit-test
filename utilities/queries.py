a8f2d8ff074c1556e76d084f96bd4aa34f522b71
Jeremy Shank <jpshank2@gmail.com>
2023-02-24 15:41:50 -0600

    fixed return statement and removing threshold to test selecting only needed columns

--- utilities/queries.py
+++ utilities/queries.py
@@ -5,25 +5,24 @@ from math import ceil
 @cache_data(ttl=3600)
 def run_query(query, _conn, rows):
     try:
-        iterations = ceil(rows / 10000)
-        finalList = list()
-        for i in range(iterations):
-            offset = i * 10000
-            iter_query = query + f'\nLIMIT 100000 OFFSET {offset};'
-            with _conn.cursor() as cur:
-                cur.execute(iter_query)
-                rows = cur.fetchall()
-                columns = [column[0] for column in cur.description]
-                results = list()
-                for row in rows:
-                    results.append(dict(zip(columns, row)))
+        # iterations = ceil(rows / 10000)
+        # finalList = list()
+        # for i in range(iterations):
+        #     offset = i * 10000
+        #     iter_query = query + f'\nLIMIT 100000 OFFSET {offset};'
+        with _conn.cursor() as cur:
+            cur.execute(query)
+            rows = cur.fetchall()
+            columns = [column[0] for column in cur.description]
+            results = list()
+            for row in rows:
+                results.append(dict(zip(columns, row)))
                 
-                finalList.append(results)
+                # finalList += results
+        return DataFrame(results)
     except Exception as e:
         iter_query = query + f'\nLIMIT 10000 OFFSET 0;'
         return {'query': iter_query, 'e': e}
-            
-    return DataFrame(finalList)
     
 def get_row_nums(table, _conn):
     with _conn.cursor() as cur:
