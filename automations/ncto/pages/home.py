205868395bed3e488870298c94649e053af1641b
Jeremy Shank <jpshank2@gmail.com>
2023-02-28 09:57:43 -0600

    attempting to add trailing whitespace for line break

--- automations/ncto/pages/home.py
+++ automations/ncto/pages/home.py
@@ -1,13 +1,13 @@
 def screen(st):
     st.write("### Required information before you begin the Client Take On Form")
 
-    st.markdown("""Name of Client
-    Primary Contact
-    Name
-    Address
-    Phone Number
-    Email Address
-    Client Partner
-    Client Manager
-    Entity
+    st.markdown("""Name of Client  
+    Primary Contact  
+    Name  
+    Address  
+    Phone Number  
+    Email Address  
+    Client Partner  
+    Client Manager  
+    Entity  
     Invoice Method""")
