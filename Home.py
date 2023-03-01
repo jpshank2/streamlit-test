2e022f514ede8ea5ca11b264d95588bcac82531b
Jeremy Shank <jpshank2@gmail.com>
2023-03-01 14:18:23 -0600

    changed hello on main page and changed page name to Home

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@
 
 import streamlit as st
 
-st.write("# Hello, home! :wave:")
+st.write("# Hello, jps-playground! :wave:")
 st.snow()
 
 st.write("This is the Abacus Technologies test site for creating streamlit apps for Business Intelligence. If you are interested in our services, please [email us](mailto:bizintel@abacustechnologies.com?subject=Streamlit).")
