1c0c436cedcb671144c39bd6399003d8c0b46331
Jeremy Shank <jpshank2@gmail.com>
2023-03-02 12:51:28 -0600

    testing using cache_data on queries coupled with from_records method for dataframes

--- utilities/queries.py
+++ utilities/queries.py
@@ -1,16 +1,16 @@
-from streamlit import cache_resource
+from streamlit import cache_data
 from pandas import DataFrame
 
-@cache_resource(ttl=3600)
+@cache_data(ttl=3600)
 def run_query(query, _conn):
     try:
         with _conn.cursor() as cur:
-            cur.execute(query)
-            rows = cur.fetchall()
+            rows = cur.execute(query)
+            # rows = cur.fetchall()
             columns = [column[0] for column in cur.description]
-            results = list()
-            for row in rows:
-                results.append(dict(zip(columns, row)))
-        return DataFrame(results)
+            # results = list()
+            # for row in rows:
+            #     results.append(dict(zip(columns, row)))
+        return DataFrame.from_records(iter(rows), columns=columns)
     except Exception as e:
         return {'query': query, 'e': e}
