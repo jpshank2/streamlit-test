a16aac855b1649670f19508e54eee715a6553851
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 15:58:39 -0500

    testing out of order column sorting on details page

--- automations/ncto/pages/client.py
+++ automations/ncto/pages/client.py
@@ -3,37 +3,38 @@ from automations.ncto.validators import validate_dropdown, validate_string, vali
 def screen(st):
     st.session_state.valid = [False for i in range(9)]
 
-    # st.write(st.session_state.staff[st.session_state.staff['STAFFCLIENTRESPONSIBLE'] == True])
+    topLeft, topRight = st.colums(2)
+    bottomFirst, bottomSecond, bottomThird, bottomFourth = st.columns(4)
 
-    st.selectbox('Client Partner', [''] + [i for i in st.session_state.staff[st.session_state.staff['STAFFCLIENTRESPONSIBLE'] == True].EMPLOYEE], key='clientpartner')
+    topLeft.selectbox('Client Partner', [''] + [i for i in st.session_state.staff[st.session_state.staff['STAFFCLIENTRESPONSIBLE'] == True].EMPLOYEE], key='clientpartner')
     st.session_state.valid[0] = validate_dropdown(st.session_state.clientpartner, [''])
 
-    st.selectbox('Client Manager', [''] + [i for i in st.session_state.staff[st.session_state.staff['STAFFMANAGER'] == True].EMPLOYEE], key='clientmanager')
+    topRight.selectbox('Client Manager', [''] + [i for i in st.session_state.staff[st.session_state.staff['STAFFMANAGER'] == True].EMPLOYEE], key='clientmanager')
     st.session_state.valid[1] = validate_dropdown(st.session_state.clientmanager, [''])
     
-    st.selectbox('Client Entity', [''] + [i for i in st.session_state.entities.ENTITYNAME], key='entity')
+    topLeft.selectbox('Client Entity', [''] + [i for i in st.session_state.entities.ENTITYNAME], key='entity')
     st.session_state.valid[2] = validate_dropdown(st.session_state.entity, ['', 'Unknown'])
     
-    st.selectbox('Client Industry', [''] + [i for i in st.session_state.industries.INDUSTRY], key='industry')
+    topRight.selectbox('Client Industry', [''] + [i for i in st.session_state.industries.INDUSTRY], key='industry')
     st.session_state.valid[3] = validate_dropdown(st.session_state.industry, ['', 'Unknown'])
-
-    st.text_input('Client Country', 'United States', key='clientcountry')
-    st.session_state.valid[8] = validate_string(st.session_state.clientcountry, [''])
 
     st.text_input('Client Address', 'Client Address', key='clientaddress')
     st.session_state.valid[4] = validate_string(st.session_state.clientaddress, ['Client Address'])
 
-    st.text_input('Client City', 'Client City', key='clientcity')
+    bottomFourth.text_input('Client Country', 'United States', key='clientcountry')
+    st.session_state.valid[8] = validate_string(st.session_state.clientcountry, [''])
+
+    bottomFirst.text_input('Client City', 'Client City', key='clientcity')
     st.session_state.valid[5] = validate_string(st.session_state.clientcity, ['Client City'])
 
     if st.session_state.clientcountry == 'United States':
-        st.selectbox('Client State', ["", "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"], key='clientstate')
+        bottomSecond.selectbox('Client State', ["", "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"], key='clientstate')
         st.session_state.valid[6] = validate_dropdown(st.session_state.clientstate, [''])
     else:
         st.text_input('Client Province', 'Client Province', key='clientprovince')
-        st.session_state.valid[6] = validate_string(st.session_state.clientprovince, ['Client Province'])
+        bottomSecond.session_state.valid[6] = validate_string(st.session_state.clientprovince, ['Client Province'])
 
-    st.text_input('Client Zip Code', 'Client Zip', key='clientzip')
+    bottomThird.text_input('Client Zip Code', 'Client Zip', key='clientzip')
     st.session_state.valid[7] = validate_nums(st.session_state.clientzip, 10000, 99999)
 
     # st.selectbox('Client Office', [''] + [i for i in st.session_state.offices.OFFICENAME], key='office')
