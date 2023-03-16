0111fbe3ca0d77a524901a81c78f41db0da74516
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 11:28:38 -0500

    testing state mutation

--- automations/ncto/pages/general.py
+++ automations/ncto/pages/general.py
@@ -2,20 +2,29 @@ from utilities.queries import run_query
 from automations.ncto.validators import validate_dropdown
 
 def screen(st, conn):
-    #st.session_state.valid = [False, False, False, False]
+    state = st.session_state
+    state.valid = [False, False, False, False]
 
     offices = run_query('SELECT * FROM DIM_OFFICES WHERE OFFICEINDEX BETWEEN 1 AND 4;', conn)
     st.write(offices[["OFFICENAME", "OFFICEINDEX"]])
 
-    type_select = st.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type')
-    office_select = st.selectbox('Client Office', [''] + [i for i in offices.OFFICENAME], key='office')
-    client_input = st.text_input('Client Name', 'Client Name', key='client')
+    st.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type')
+    state.valid[0] = validate_dropdown(state.type, [''])
 
-    if type_select == "New Client Relationship":
+    st.selectbox('Client Office', [''] + [i for i in offices.OFFICENAME], key='office')
+    state.valid[1] = validate_dropdown(state.office, [''])
+
+    st.text_input('Client Name', 'Client Name', key='client')
+    state.valid[2] = validate_dropdown(state.client, ['', 'Client Name'])
+
+    if state.type == "New Client Relationship":
         originators = run_query("SELECT * FROM DIM_STAFF_MASTER WHERE STAFF_STATUS = 'ACTIVE' AND DEPARTMENT <> 'No Selection';", conn)
-        originator_select = st.selectbox('Originator', [''] + [i for i in originators.EMPLOYEE], key='originator')
+        st.selectbox('Originator', [''] + [i for i in originators.EMPLOYEE], key='originator')
+        state.valid[3] = validate_dropdown(state.originator, [''])
     else:
         clients = run_query("SELECT * FROM DIM_CLIENT_MASTER WHERE STATUS <> 'LOST';", conn)
-        relationship_select = st.selectbox('Client Relationship', [''] + [i for i in clients.CLIENTDISPLAY], key='relationship')
+        st.selectbox('Client Relationship', [''] + [i for i in clients.CLIENTDISPLAY], key='relationship')
+        state.valid[3] = validate_dropdown(state.relationship, [''])
 
-    st.markdown(st.session_state)
+
+    st.markdown(state)
