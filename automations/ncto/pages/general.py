c3a83f22b2088bd83c6a4c007c698e33f42cf2d9
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 15:53:01 -0500

    testing columns on general page of nct

--- automations/ncto/pages/general.py
+++ automations/ncto/pages/general.py
@@ -3,19 +3,21 @@ from automations.ncto.validators import validate_dropdown, validate_string
 def screen(st):
     st.session_state.valid = [False for i in range(4)]
 
-    st.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type')
+    leftCol, rightCol = st.columns(2)
+
+    leftCol.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type')
     st.session_state.valid[0] = validate_dropdown(st.session_state.type, [''])
 
-    st.selectbox('Client Office', [''] + [i for i in st.session_state.offices.OFFICENAME], key='office')
+    rightCol.selectbox('Client Office', [''] + [i for i in st.session_state.offices.OFFICENAME], key='office')
     st.session_state.valid[1] = validate_dropdown(st.session_state.office, [''])
 
-    st.text_input('Client Name', 'Client Name', key='client')
+    leftCol.text_input('Client Name', 'Client Name', key='client')
     st.session_state.valid[2] = validate_string(st.session_state.client, ['Client Name'])
 
     if st.session_state.type == "New Client Relationship":
-        st.selectbox('Originator', [''] + [i for i in st.session_state.staff.EMPLOYEE], key='originator')
+        rightCol.selectbox('Originator', [''] + [i for i in st.session_state.staff.EMPLOYEE], key='originator')
         st.session_state.valid[3] = validate_dropdown(st.session_state.originator, [''])
     elif st.session_state.type == 'Subcode Needed for Existing Client':
-        st.selectbox('Client Relationship', [''] + [i for i in st.session_state.clients.CLIENTDISPLAY], key='relationship')
+        rightCol.selectbox('Client Relationship', [''] + [i for i in st.session_state.clients.CLIENTDISPLAY], key='relationship')
         st.session_state.valid[3] = validate_dropdown(st.session_state.relationship, [''])
 
