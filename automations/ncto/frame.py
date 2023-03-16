f6230312d832cb94b445eb08e0ea984282f2670b
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 08:59:15 -0500

    should be disabled=True when disabled not other way around

--- automations/ncto/frame.py
+++ automations/ncto/frame.py
@@ -30,7 +30,7 @@ def clientTakeOn(st, conn):
     if st.session_state.pageCounter > 0:
         st.button('Previous Page', on_click=prev_page, args=(st,))
 
-    st.button('Save & Next', key=(f'button-{st.session_state.pageCounter}'), on_click=next_page, args=(st,), disabled=(False not in st.session_state['valid']))
+    st.button('Save & Next', key=(f'button-{st.session_state.pageCounter}'), on_click=next_page, args=(st,), disabled=(False in st.session_state['valid']))
 
     progress_bar = st.progress(0)
     progress_bar.progress(int(100 * (st.session_state.pageCounter / len(pageList))))
