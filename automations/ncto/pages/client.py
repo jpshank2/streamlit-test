64593c3b57de6f4248abe938d7d4a40e01242aa7
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 15:43:02 -0500

    adding address fields for client details

--- automations/ncto/pages/client.py
+++ automations/ncto/pages/client.py
@@ -1,4 +1,4 @@
-from automations.ncto.validators import validate_dropdown, validate_string
+from automations.ncto.validators import validate_dropdown, validate_string, validate_nums
 
 def screen(st):
     st.session_state.valid = [False for i in range(9)]
@@ -17,7 +17,24 @@ def screen(st):
     st.selectbox('Client Industry', [''] + [i for i in st.session_state.industries.INDUSTRY], key='industry')
     st.session_state.valid[3] = validate_dropdown(st.session_state.industry, ['', 'Unknown'])
 
+    st.text_input('Client Address', 'Client Address', key='clientaddress')
+    st.session_state.valid[4] = validate_string(st.session_state.clientaddress, ['Client Address'])
 
+    st.text_input('Client City', 'Client City', key='clientcity')
+    st.session_state.valid[5] = validate_string(st.session_state.clientcity, ['Client City'])
+
+    if st.session_state.country == 'United States':
+        st.selectbox('Client State', ["", "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"], key='clientstate')
+        st.session_state.valid[6] = validate_dropdown(st.session_state.clientstate, [''])
+    else:
+        st.text_input('Client Province', 'Client Province', key='clientprovince')
+        st.session_state.valid[6] = validate_string(st.session_state.clientprovince, ['Client Province'])
+
+    st.text_input('Client Zip Code', 'Client Zip', key='clientzip')
+    st.session_state.valid[7] = validate_nums(st.session_state.clientzip, 10000, 99999)
+
+    st.text_input('Client Country', 'Client Country', key='clientcountry')
+    st.session_state.valid[8] = validate_string(st.session_state.clientcountry, [''])
     # st.selectbox('Client Office', [''] + [i for i in st.session_state.offices.OFFICENAME], key='office')
     # st.session_state.valid[1] = validate_dropdown(st.session_state.office, [''])
 
--- automations/ncto/validators.py
+++ automations/ncto/validators.py
@@ -10,3 +10,10 @@ def validate_string(input, notAllowed):
     else:
         return False if input in notAllowed else True
 
+def validate_nums(input, minVal, maxVal):
+    from re import search
+
+    if search('\D', input) == None and len(input) >= minVal and len(input) <= maxVal:
+        return True
+    else:
+        return True if len(input) >= minVal and len(input) <= maxVal else False
