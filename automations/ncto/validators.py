36c6b1c96e2107d50412988a2ea4c5779ee616b8
Jeremy Shank <jpshank2@gmail.com>
2023-03-16 20:45:55 -0500

    testing validate_nums

--- automations/ncto/validators.py
+++ automations/ncto/validators.py
@@ -11,11 +11,15 @@ def validate_string(input, notAllowed):
         return False if input in notAllowed else True
 
 def validate_nums(input, minVal, maxVal):
+    print(input)
     from re import search
 
-    if search('\w', input):
+    if search('[^0-9]+', input) != None:
         return False
-    elif search('\D', input) == None and int(input) >= minVal and int(input) <= maxVal:
-        return True
+    elif search('\D', input) == None:
+        if int(input) >= minVal and int(input) <= maxVal:
+            return True
+        else:
+            return False
     else:
         return True if int(input) >= minVal and int(input) <= maxVal else False
