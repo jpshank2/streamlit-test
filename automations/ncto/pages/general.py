2bddce628b0c6dffc948f26b6fc4fc21166d80ec
Jeremy Shank <jpshank2@gmail.com>
2023-03-17 11:28:31 -0500

    moving warnings to expander block

--- automations/ncto/pages/general.py
+++ automations/ncto/pages/general.py
@@ -1,39 +1,28 @@
 from utilities.validators import validate_dropdown, validate_string
-# from utilities.click_handlers import inc_click
+# from utilities.click_handlers import inc_click - want to use to only render warnings after 
 
 def screen(st):
-
-    def inc_click(position):
-        updatedClicks = st.session_state.clicks
-        updatedClicks[position] = updatedClicks[position] + 1
-        st.session_state.clicks = updatedClicks
-
     st.session_state.valid = [False for i in range(4)]
     st.session_state.clicks = [0 for i in range(4)]
 
     leftCol, rightCol = st.columns(2)
 
-    leftCol.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type', on_change=(lambda: st.session_state.clicks[0] + 1))
+    leftCol.selectbox('New or Existing Client?', ('', 'New Client Relationship', 'Subcode Needed for Existing Client'), key='type')
     st.session_state.valid[0] = validate_dropdown(st.session_state.type, [''])
 
-    existingWarning = st.empty()
+    warnings = st.expander('View warnings')
 
-    if not st.session_state.valid[0] and st.session_state.clicks[0] > 0:
-        with existingWarning.container():
+    if not st.session_state.valid[0]:
+        with warnings:
             st.warning('Please select either new or existing client!')
-    elif st.session_state.clicks[0] == 0:
-        with existingWarning.container():
-            st.markdown('<br style="margin:8px;">', unsafe_allow_html=True)
-    else:
-        existingWarning.empty()
-        with existingWarning.container():
-            st.markdown('')
+    # else:
+    #     existingWarning.empty()
 
 
     rightCol.selectbox('Client Office', [''] + [i for i in st.session_state.offices.OFFICENAME], key='office')
     st.session_state.valid[1] = validate_dropdown(st.session_state.office, [''])
 
-    officeWarning = rightCol.empty()
+    officeWarning = st.empty()
 
     if not st.session_state.valid[1]:
         with officeWarning.container():
@@ -44,7 +33,7 @@ def screen(st):
     leftCol.text_input('Client Name', 'Client Name', key='client')
     st.session_state.valid[2] = validate_string(st.session_state.client, ['Client Name'])
 
-    nameWarning = leftCol.empty()
+    nameWarning = st.empty()
 
     if not st.session_state.valid[2]:
         with nameWarning.container():
@@ -56,7 +45,7 @@ def screen(st):
         rightCol.selectbox('Originator', [''] + [i for i in st.session_state.staff.EMPLOYEE], key='originator')
         st.session_state.valid[3] = validate_dropdown(st.session_state.originator, [''])
 
-        originatorWarning = rightCol.empty()
+        originatorWarning = st.empty()
 
         if not st.session_state.valid[3]:
             with originatorWarning.container():
@@ -68,12 +57,10 @@ def screen(st):
         rightCol.selectbox('Client Relationship', [''] + [i for i in st.session_state.clients.CLIENTDISPLAY], key='relationship')
         st.session_state.valid[3] = validate_dropdown(st.session_state.relationship, [''])
 
-        existingWarning = rightCol.empty()
+        relationshipWarning = st.empty()
 
         if not st.session_state.valid[3]:
-            with existingWarning.container():
+            with relationshipWarning.container():
                 st.warning('Please select a client relationship for this client!')
         else:
-            existingWarning.empty()
-
-    st.write(st.session_state.clicks)
+            relationshipWarning.empty()
