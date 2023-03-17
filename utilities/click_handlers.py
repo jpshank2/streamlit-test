68b7656a5dbf15a251524468c04bfb9c707be927
Jeremy Shank <jpshank2@gmail.com>
2023-03-17 10:52:43 -0500

    trying to rewrite session in external func

--- utilities/click_handlers.py
+++ utilities/click_handlers.py
@@ -1,4 +1,5 @@
 def inc_click(st, position):
-    st.session_state.clicks[position] = st.session_state.clicks[position] + 1
+    updatedClicks = st.session_state.clicks
+    updatedClicks[position] = updatedClicks[position] + 1
 
-    st.write(st.session_state.clicks[position])
+    st.session_state.clicks = updatedClicks
