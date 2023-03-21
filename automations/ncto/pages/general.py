b2ec19e94b3524b182111cac343bb2f9451f6758
Jeremy Shank <jpshank2@gmail.com>
2023-03-21 14:55:14 -0500

    correcting column names in dataframe queries on general page

--- automations/ncto/pages/general.py
+++ automations/ncto/pages/general.py
@@ -29,9 +29,9 @@ def screen(st):
     if not st.session_state.valid[2]:
         with warnings:
             st.warning('Please enter a valid name for this client!')
-    elif st.session_state.clients[st.session_state.clients['CLIENTNAME'].str.upper() == st.session_state.general_client.upper()].shape[0] > 0:
+    elif st.session_state.clients[st.session_state.clients['CLIENT'].str.upper() == st.session_state.general_client.upper()].shape[0] > 0:
         with warnings:
-            st.warning('A client with this name already exists! - ' + st.session_state.clients[st.session_state.clients['CLIENTNAME'].str.upper() == st.session_state.general_client.upper()].CLIENTDISPLAY)
+            st.warning('A client with this name already exists! - ' + st.session_state.clients[st.session_state.clients['CLIENT'].str.upper() == st.session_state.general_client.upper()].CLIENTDISPLAY)
 
     if st.session_state.general_type == "New Client Relationship":
         rightCol.selectbox('Originator', [''] + [i for i in st.session_state.staff.EMPLOYEE], key='general_originator')
