67a5fac39b5affc224b867dffbabc1c6ae728f50
Jeremy Shank <jpshank2@gmail.com>
2023-03-24 16:18:10 -0500

    toggling services based on entity type

--- automations/ncto/pages/attributes.py
+++ automations/ncto/pages/attributes.py
@@ -4,7 +4,14 @@ from utilities.validators import validate_multi
 def screen(st):
     st.session_state.valid = [False]
 
-    st.multiselect('Client Services', st.session_state.services.SERVTITLE, key='attributes_service')
+    st.write(st.session_state.services)
+
+    if st.session_state.newclient['client'][-1]['client_entity'] == 'INDIVIDUAL':
+        available_servs = st.session_state.services[st.session_state.services['SERVPERSON'] == True]
+    else:
+        available_servs = st.session_state.services[st.session_state.services['SERVORG'] == True]
+
+    st.multiselect('Client Services', available_servs.SERVTITLE, key='attributes_service')
     st.session_state.valid[0] = validate_multi(st.session_state.attributes_service)
 
     warnings = st.expander('View warnings')
