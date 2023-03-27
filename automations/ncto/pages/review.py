2668e7c0c9b5b1178f361d158ff41713e17fcb3f
Jeremy Shank <jpshank2@gmail.com>
2023-03-27 13:47:15 -0500

    adding most of remaining review screen and adding test redo section for general page

--- automations/ncto/pages/review.py
+++ automations/ncto/pages/review.py
@@ -6,17 +6,16 @@ def screen(st):
     attributes = st.session_state['newclient']['attributes'][-1]
     allServices = st.session_state['newclient']['services']
 
-    topOne, topTwo, topThree, topFour, topFive, topSix, topSeven = st.columns(7)
+    topOne, topTwo, topThree, topFour = st.columns(4)
+    upperMidOne, upperMidTwo, upperMidThree = st.columns(3)
     midOne, midTwo, midThree, midFour = st.columns(4)
     botOne, botTwo, botThree = st.columns(3)
 
-    with topOne.container():
-        st.markdown(general['general_client'])
-        st.caption('Client Name')
+    topOne.markdown(general['general_client'])
+    topOne.caption('Client Name')
 
-    with topTwo.container():
-        st.markdown(general['general_office'])
-        st.caption('Client Office')
+    topTwo.markdown(general['general_office'])
+    topTwo.caption('Client Office')
 
     topThree.markdown(client['client_partner'])
     topThree.caption('Client Partner')
@@ -24,11 +23,33 @@ def screen(st):
     topFour.markdown(client['client_manager'])
     topFour.caption('Client Manager')
 
-    topFive.markdown(general['general_originator']) if general['general_type'] == 'New Client Relationship' else topFive.markdown(general['general_relationship'])
-    topFive.caption('Client Originator') if general['general_type'] == 'New Client Relationship' else topFive.caption('Client Relationship')
+    upperMidOne.markdown(general['general_originator']) if general['general_type'] == 'New Client Relationship' else upperMidOne.markdown(general['general_relationship'])
+    upperMidOne.caption('Client Originator') if general['general_type'] == 'New Client Relationship' else upperMidOne.caption('Client Relationship')
 
-    topSix.markdown(client['client_entity'])
-    topSix.caption('Client Entity')
+    upperMidTwo.markdown(client['client_entity'])
+    upperMidTwo.caption('Client Entity')
 
-    topSeven.markdown(client['client_industry'])
-    topSeven.caption('Client Industry')
+    upperMidThree.markdown(client['client_industry'])
+    upperMidThree.caption('Client Industry')
+
+    midOne.markdown(contact['contact_address'] + '\n' + contact['contact_city'] + ', ' + contact['contact_state'] if contact['contact_country'] == 'United States' else contact['contact_province'] + ' ' + contact['contact_zip'] + ' ' + contact['contact_country'])
+    midOne.caption('Contact Address')
+
+    midTwo.markdown(billings['billings_address'] + '\n' + billings['billings_city'] + ', ' + billings['billings_state'] if billings['billings_country'] == 'United States' else billings['billings_province'] + ' ' + billings['billings_zip'] + ' ' + billings['billings_country'])
+    midTwo.caption('Billing Address')
+
+    midThree.markdown(client['client_address'] + '\n' + client['client_city'] + ', ' + client['client_state'] if client['client_country'] == 'United States' else client['client_province'] + ' ' + client['client_zip'] + ' ' + client['client_country'])
+    midThree.caption('Client Address')
+
+    midFour.markdown(contact['contact_phone'] if contact['contact_phone'] != 'Numbers Only' and contact['contact_phone'] != '' else 'No phone number listed')
+    midFour.caption('Contact Phone Number')
+
+    botOne.markdown('Create services table')
+
+    botTwo.markdown(contact['contact_email'])
+    botTwo.caption('Contact Email')
+
+    botTwo.markdown(billings['billings_invoice'])
+    botTwo.caption('Client Invoice Method')
+
+    botThree.text_input('Client Notes', placeholder='Enter Any Additional Notes, Jobs to be added, etc.')
