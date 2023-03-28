4ffcb5a75225f285e7bec832ce20ba5ae3cb0e1b
Jeremy Shank <jpshank2@gmail.com>
2023-03-28 15:16:14 -0500

    removing written pagelist

--- automations/ncto/frame.py
+++ automations/ncto/frame.py
@@ -42,8 +42,6 @@ def clientTakeOn(st):
 
     try:
         pageList = loads(st.session_state['switches'].NCTO_PAGES.iloc[0])
-        st.write(pageList)
-        # st.write(pageList.split('}, '))
     except:
         pageList = [{'name': 'General Information', 'module': 'automations.ncto.pages.general'}, {'name': 'Client Details', 'module': 'automations.ncto.pages.client'}, {'name': 'Contact Details', 'module': 'automations.ncto.pages.contact'}, {'name': 'Client Billings', 'module': 'automations.ncto.pages.billings'}, {'name': 'Client Attributes', 'module': 'automations.ncto.pages.attributes'}, {'name': 'Services', 'module': 'automations.ncto.pages.services'}, {'name': 'Final Review', 'module': 'automations.ncto.pages.review'}, {'name': 'Submitted', 'module': 'automations.ncto.pages.end'}]
     
