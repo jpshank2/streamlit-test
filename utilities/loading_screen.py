08ba8a30ad67434d688a64b414bace07770622be
Jeremy Shank <jpshank2@gmail.com>
2023-04-11 14:04:59 -0500

    working on loading screen

--- utilities/loading_screen.py
+++ utilities/loading_screen.py
@@ -2,8 +2,6 @@ from utilities.queries import validation_connection, get_rows, init_connection
 def loading(st):
     email = 'jeremyshank@bmss.com' if st.experimental_user.email == 'jpshank2@gmail.com' else st.experimental_user.email
     domain = email.split('@')[-1]
-    st.write(email)
-    st.write(domain)
 
     with st.spinner('Curating your experience...'):
         conn = validation_connection()
@@ -12,7 +10,7 @@ def loading(st):
             st.markdown("We're sorry, you are not a registered company with Magnify")
             st.markdown("Please [contact us](mailto:bizintel@abacustechnologies.com?subject=Join Magnify) to join the Magnify community")
         else:
-            st.markdown('Welcome to Magnify!')
+            st.markdown('# Welcome to Magnify!')
             if 'company' not in st.session_state:
                 st.session_state['company'] = company
             if 'conn' not in st.session_state:
