ced78f280d36e9daf0f31d1c67a86dce6678151f
Jeremy Shank <jpshank2@gmail.com>
2023-04-11 14:49:48 -0500

    changing to three columns

--- Magnify.py
+++ Magnify.py
@@ -15,8 +15,8 @@ st.set_page_config(
 )
 
 MainHeaderImage = Image.open(BytesIO(get('https://i.imgur.com/khnCmv8.png').content))
-one, two, three, four, five = st.columns(5)
-three.image(MainHeaderImage, use_column_width = True)
+one, two, three= st.columns(3)
+two.image(MainHeaderImage, use_column_width = True)
 
 if 'company' not in st.session_state:
     loading(st)
