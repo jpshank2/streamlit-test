defc1d577a759db72c41ee337477cd7cc91bb4d9
Jeremy Shank <jpshank2@gmail.com>
2023-04-12 14:15:30 -0500

    writing email to screen

--- utilities/loading_screen.py
+++ utilities/loading_screen.py
@@ -1,6 +1,7 @@
 from utilities.queries import validation_connection, get_rows, init_connection
 
 def loading(st):
+    st.write(st.experimental_user.email)
     if st.experimental_user.email == 'jpshank2@gmail.com':
         email = 'jeremyshank.com'
     elif st.experimental_user.email == 'jdavidbrowncpa@gmail.com':
