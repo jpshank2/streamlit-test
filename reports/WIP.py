6fc1f9e70ec63e5d67d83d9ab60c87a464267c59
Jeremy Shank <jpshank2@gmail.com>
2023-04-14 17:36:52 -0500

    last push for day, correctly selecting columsn

--- reports/WIP.py
+++ reports/WIP.py
@@ -40,7 +40,7 @@ def level_1_wip(st):
         cy_util_df = cy_util_df.groupby('STAFFINDEX').agg(TOTAL_HOURS=('WIPHOURS', 'sum'), BILLABLE_HOURS=('BILLABLEHOURS', 'sum'), NON_BILL_HOURS=('NONBILLABLEHOURS', 'sum')).reset_index()
         cy_util_df['UTILIZATION'] = f"{round((cy_util_df['BILLABLE_HOURS'] / cy_util_df['TOTAL_HOURS']) * 100, 2)}%"
 
-        cy_col.write(cy_util_df['TOTAL_HOURS', 'BILLABLE_HOURS', 'NON_BILL_HOURS', 'UTILIZATION'])
+        cy_col.write(cy_util_df[['TOTAL_HOURS', 'BILLABLE_HOURS', 'NON_BILL_HOURS', 'UTILIZATION']])
         
         # cy_wip_df = wip_df
         # py_wip_df = wip_df
