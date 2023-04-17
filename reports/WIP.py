e9d6aa1f768c5f20171e16c58929fddb51605479
Jeremy Shank <jpshank2@gmail.com>
2023-04-17 09:20:44 -0500

    formatting cy util df

--- reports/WIP.py
+++ reports/WIP.py
@@ -40,9 +40,12 @@ def level_1_wip(st):
 
         cy_util_df = cy_wip_df[['STAFFINDEX', 'WIPHOURS', 'BILLABLEHOURS', 'NONBILLABLEHOURS']]
         cy_util_df = cy_util_df.groupby('STAFFINDEX').agg(TOTAL_HOURS=('WIPHOURS', 'sum'), BILLABLE_HOURS=('BILLABLEHOURS', 'sum'), NON_BILL_HOURS=('NONBILLABLEHOURS', 'sum')).reset_index()
-        cy_util_df['UTILIZATION'] = round((cy_util_df['BILLABLE_HOURS'] / cy_util_df['TOTAL_HOURS']) * 100, 2)
-        cy_util_df['UTILIZATION'] = cy_util_df['UTILIZATION'].astype(str)
-        cy_util_df['UTILIZATION'] = cy_util_df['UTILIZATION'] + '%'
+        cy_util_df['UTILIZATION'] = round((cy_util_df['BILLABLE_HOURS'] / cy_util_df['TOTAL_HOURS']) * 100, 2).astype(str) + '%'
+        cy_util_df.style.format({
+            'TOTAL_HOURS': "{:.2f}",
+            'BILLABLE_HOURS': "{:.2f}",
+            'NON_BILL_HOURS': "{:.2f}",
+        }).hide_index()
 
         cy_col.dataframe(cy_util_df[['TOTAL_HOURS', 'BILLABLE_HOURS', 'NON_BILL_HOURS', 'UTILIZATION']], use_container_width=True)
 
