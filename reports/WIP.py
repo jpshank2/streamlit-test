eb74627ce11ce2eb1e43e7c17124e90019d30d71
Jeremy Shank <jpshank2@gmail.com>
2023-04-17 09:25:59 -0500

    assigning df style

--- reports/WIP.py
+++ reports/WIP.py
@@ -41,7 +41,7 @@ def level_1_wip(st):
         cy_util_df = cy_wip_df[['STAFFINDEX', 'WIPHOURS', 'BILLABLEHOURS', 'NONBILLABLEHOURS']]
         cy_util_df = cy_util_df.groupby('STAFFINDEX').agg(TOTAL_HOURS=('WIPHOURS', 'sum'), BILLABLE_HOURS=('BILLABLEHOURS', 'sum'), NON_BILL_HOURS=('NONBILLABLEHOURS', 'sum')).reset_index()
         cy_util_df['UTILIZATION'] = round((cy_util_df['BILLABLE_HOURS'] / cy_util_df['TOTAL_HOURS']) * 100, 2).astype(str) + '%'
-        cy_util_df.style.format({
+        cy_util_df = cy_util_df.style.format({
             'TOTAL_HOURS': "{:.2f}",
             'BILLABLE_HOURS': "{:.2f}",
             'NON_BILL_HOURS': "{:.2f}"
