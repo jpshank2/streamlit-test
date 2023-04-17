f6408bc4d3662bce4bf12a2d60c798e51945a568
Jeremy Shank <jpshank2@gmail.com>
2023-04-17 15:47:16 -0500

    writing wip service colors

---
+++ utilities/__init__.py
@@ -0,0 +1,18 @@
+color_map = {
+"SERVICE":{
+"4":"Business Tax",
+"5":"Transaction Advisory Services",
+"6":"Client Accounting Services",
+"10":"Government Contract Consulting",
+"19":"Individual Tax",
+"20":"MAS",
+},
+"COLOR":{
+"4":"#3333ff",
+"5":"#999900",
+"6":"#00cc66",
+"10":"#1a1aff",
+"19":"#669999",
+"20":"#cc3300",
+}
+}
