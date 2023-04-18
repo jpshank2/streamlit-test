c54d5d439b83ffe66664084044e7c224f758fdd3
Jeremy Shank <jpshank2@gmail.com>
2023-04-18 11:08:31 -0500

    updating wip query to include staff levels

--- pages/Magnify Reports.py
+++ pages/Magnify Reports.py
@@ -70,11 +70,28 @@ where DEBTTRANDATE >= date_from_parts(year(current_timestamp) - 3, 1, 1);""")
         st.markdown('### WIP Reports')
         go_to_top()
         if 'wip' not in st.session_state:
-            st.session_state['wip'] = get_rows("""SELECT WIP.WIPOUTSTANDING, WIP.STAFFINDEX, WIP.WIPHOURS, WIP.WIPAMOUNT, WIP.WIPBILLED, WIP.CONTINDEX, WIP.WIPDATE, WIP.SERVICETITLE, CASE WHEN WIP.CONTINDEX < 900000 THEN WIP.WIPHOURS ELSE 0 END AS BILLABLEHOURS, CASE WHEN WIP.CONTINDEX >= 900000 THEN WIP.WIPHOURS ELSE 0 END AS NONBILLABLEHOURS, C.CLIENT_PARTNER, C.CLIENT, C.OFFICE, D.AGING_PERIOD_SORT, D.AGING_PERIOD as OG_PERIOD, D.MONTH_NAME AS MONTH
-            from TRANS_WIP WIP
-                INNER JOIN DIM_CLIENT_MASTER C ON C.ContIndex = WIP.ContIndex 
-                INNER JOIN DIM_DATES D ON D.CALENDAR_DATE = WIP.WIPDATE
-            WHERE WIPDATE >= date_from_parts(year(current_timestamp) - 3, 1, 1);""")
+            st.session_state['wip'] = get_rows("""SELECT WIP.WIPOUTSTANDING, 
+    WIP.STAFFINDEX,
+    S.LEVEL, 
+    WIP.WIPHOURS, 
+    WIP.WIPAMOUNT,
+    WIP.WIPBILLED, 
+    WIP.CONTINDEX, 
+    WIP.WIPDATE, 
+    WIP.SERVICETITLE, 
+    CASE WHEN WIP.CONTINDEX < 900000 THEN WIP.WIPHOURS ELSE 0 END AS BILLABLEHOURS, 
+    CASE WHEN WIP.CONTINDEX >= 900000 THEN WIP.WIPHOURS ELSE 0 END AS NONBILLABLEHOURS, 
+    C.CLIENT_PARTNER, 
+    C.CLIENT, 
+    C.OFFICE, 
+    D.AGING_PERIOD_SORT, 
+    D.AGING_PERIOD as OG_PERIOD, 
+    D.MONTH_NAME AS MONTH
+from TRANS_WIP WIP
+    INNER JOIN DIM_CLIENT_MASTER C ON C.ContIndex = WIP.ContIndex 
+    INNER JOIN DIM_DATES D ON D.CALENDAR_DATE = WIP.WIPDATE
+    INNER JOIN DIM_STAFF_MASTER S ON S.STAFFINDEX = WIP.STAFFINDEX
+WHERE WIPDATE >= date_from_parts(year(current_timestamp) - 3, 1, 1);""")
         exec(f"""from reports.WIP import level_{st.session_state['level']}_wip as level_wip\nlevel_wip(st)""")
         # wip_report = exec(f"from reports.WIP import level_{st.session_state['level']}_wip as level_wip")
         # level_wip = getattr(wip, f"level_{st.session_state['level']}_wip")
