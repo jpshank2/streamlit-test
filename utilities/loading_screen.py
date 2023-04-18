78e7f85bdd74e561ad4dbfcd2b1b5c4fbc9d3886
Jeremy Shank <jpshank2@gmail.com>
2023-04-18 14:28:30 -0500

    filtering out non billable amounts on realization

--- utilities/loading_screen.py
+++ utilities/loading_screen.py
@@ -9,7 +9,7 @@ def loading(st):
     elif st.experimental_user.email == 'imesser@abacustechnologies.com':
         email = 'kfluker@bmss.com'
     else:
-        email = 'fsmith@bmss.com'#st.experimental_user.email
+        email = 'munderhill@bmss.com'#st.experimental_user.email
         
     domain = email.split('@')[-1]
 
@@ -26,7 +26,10 @@ def loading(st):
             if 'user' not in st.session_state:
                 st.session_state['user'] = get_rows(f"SELECT S.*, L.LEVEL_PERMISSION FROM DIM_STAFF_MASTER S INNER JOIN CONFIGURATIONS.LEVELS L ON L.LEVEL_DESC = S.LEVEL WHERE STAFF_EMAIL = '{email}'")
             if 'level' not in st.session_state:
-                st.session_state['level'] = st.session_state['user'].LEVEL_PERMISSION.iloc[0]
+                if st.session_state['user'].LEVEL_PERMISSION.iloc[0] == 4 and st.session_state['user'].DEPARTMENT.iloc[0] == 'Steering Committee':
+                    st.session_state['level'] = 5
+                else:
+                    st.session_state['level'] = st.session_state['user'].LEVEL_PERMISSION.iloc[0]
             if 'today' not in st.session_state:
                 st.session_state['today'] = datetime.today()
             if 'color_map' not in st.session_state:
