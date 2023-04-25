dd896fa8f09e23253576584a23838b1c938fd86c
Jeremy Shank <jpshank2@gmail.com>
2023-04-25 16:37:19 -0500

    adding submit button disabled

--- pages/Magnify People.py
+++ pages/Magnify People.py
@@ -47,15 +47,15 @@ if 'company' in st.session_state:
         st.session_state['review_valid'][0] = validate_dropdown(st.session_state['review_employee'], [''])
 
         st.text_input('What Job or Project are you reviewing?', value=st.session_state['project_input'], key='review_project')
-        st.session_state['review_valid'][1] = validate_string(st.session_state['review_project'], '')
+        st.session_state['review_valid'][1] = validate_string(st.session_state['review_project'], [''])
         
         st.radio('How did this staff do on the project?', ('Thumbs up', 'Okay', 'Thumbs down'), key='review_rating', horizontal=True)
         
         st.text_area('See more', placeholder='What did this co-worker do well that you\'d like to see more?', key='review_more')
-        st.session_state['review_valid'][2] = validate_string(st.session_state['review_more'], '')
+        st.session_state['review_valid'][2] = validate_string(st.session_state['review_more'], [''])
         
         st.text_area('See less', placeholder='What did this co-worker do that you\'d like to see less?', key='review_less')
-        st.session_state['review_valid'][3] = validate_string(st.session_state['review_less'], '')
+        st.session_state['review_valid'][3] = validate_string(st.session_state['review_less'], [''])
         
         st.form_submit_button('Submit', type='primary', disabled=(False in st.session_state['review_valid']))
     
