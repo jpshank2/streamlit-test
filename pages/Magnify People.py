92be81da8d4768af003d1145ca751ad2ebafbbcc
Jeremy Shank <jpshank2@gmail.com>
2023-04-25 17:03:51 -0500

    changing to text input

--- pages/Magnify People.py
+++ pages/Magnify People.py
@@ -51,7 +51,7 @@ if 'company' in st.session_state:
         
         st.radio('How did this staff do on the project?', ('Thumbs up', 'Okay', 'Thumbs down'), key='review_rating', horizontal=True)
         
-        review_more = st.text_area('See more')#, placeholder='What did this co-worker do well that you\'d like to see more?')
+        review_more = st.text_input('See more')#, placeholder='What did this co-worker do well that you\'d like to see more?')
         st.write(review_more)
         st.session_state['review_valid'][2] = validate_string(review_more, [''])
         
