84ff2493f8b9dccb4c4fa774680f38663b0fc13b
Jeremy Shank <jpshank2@gmail.com>
2023-04-26 16:12:08 -0500

    adding base plotly chart

--- reports/people.py
+++ reports/people.py
@@ -34,7 +34,7 @@ def level_4_people(st):
     boost_viz.plotly_chart(boost_fig, use_container_width=True)
     boost_tab.dataframe(boost_df[['EMPLOYEE', 'DATE', 'ENTHUSIASM', 'MEANING', 'PRIDE', 'CHALLENGE', 'ENERGY', 'STRONG', 'RECOVERY', 'ENDURANCE', 'AGG', 'TOTAL_HOURS', 'UTILIZATION']])
 
-    morale_df = get_rows("""select s.staff_name, s.level, m.*, enthusiasm + meaning + pride + challenge + energy + strong + recovery + endurance as agg, month(m.date) as month
+    morale_df = get_rows("""select s.staff_name, s.level, m.*, enthusiasm + meaning + pride + challenge + energy + strong + recovery + endurance as agg, monthname(m.date) as month
         from people.morale m
             inner join dim_staff_master s on s.staffindex = m.staff
         where s.staff_status = 'Active' and
@@ -55,5 +55,12 @@ def level_4_people(st):
         morale_df = morale_df[(morale_df['LEVEL'] == level_select) & (morale_df['STAFF_NAME'] == staff_select)]
     else:
         morale_df = morale_df
-    
-    st.dataframe(morale_df)
+        grouped_morale_df = morale_df[['LEVEL', 'MONTH', 'ENTHUSIASM', 'MEANING', 'PRIDE', 'CHALLENGE', 'ENERGY', 'STRONG', 'RECOVERY', 'ENDURANCE', 'AGG']].groupby(['LEVEL', 'MONTH']).agg(AVERAGE_ENTHUSIASM=('ENTHUSIASM', 'mean'), AVERAGE_MEANING=('MEANING', 'mean'), AVERAGE_PRIDE=('PRIDE', 'mean'), AVERAGE_CHALLENGE=('CHALLENGE', 'mean'), AVERAGE_ENERGY=('ENERGY', 'mean'), AVERAGE_STRENGTH=('STRONG', 'mean'), AVERAGE_RECOVERY=('RECOVERY', 'mean'), AVERAGE_ENDURANCE=('ENDURANCE', 'mean'), AVERAGE_AGG=('AGG', 'mean'))
+        color_val = 'LEVEL'
+        hover_data = {'LEVEL': False, 'AVERAGE_AGG': True, 'AVERAGE_ENTHUSIASM': True, 'AVERAGE_MEANING': True, 'AVERAGE_PRIDE': True, 'AVERAGE_CHALLENGE': True, 'AVERAGE_ENERGY': True, 'AVERAGE_STRENGTH': True, 'AVERAGE_RECOVERY': True, 'AVERAGE_ENDURANCE': True}
+        title = 'Average Morale by Level and Month'
+
+    morale_viz, morale_tab = st.tabs(['Visual', 'Table'])
+
+    morale_viz.plotly_chart(line(grouped_morale_df, x='MONTH', y='AGG', color=color_val, markers=True, hover_name=color_val, hover_data=hover_data, title=title))    
+    morale_tab.dataframe(morale_df[['STAFF_NAME', 'LEVEL', 'DATE', 'ENTHUSIASM', 'MEANING', 'PRIDE', 'CHALLENGE', 'ENERGY', 'STRONG', 'RECOVERY', 'ENDURANCE', 'AGG']])
