30b105735a15ca9145f037468443cb2491a54ae8
Jeremy Shank <jpshank2@gmail.com>
2023-04-26 16:35:34 -0500

    formatting date

--- reports/people.py
+++ reports/people.py
@@ -40,8 +40,8 @@ def level_4_people(st):
         where s.staff_status = 'Active' and
             s.level not in ('No Selection', 'Unknown');""")
 
-    from pandas import to_datetime
-    morale_df['DATE'] = to_datetime(morale_df['DATE'], format='%b %Y')
+    # from pandas import to_datetime
+    morale_df['DATE'] = morale_df['DATE'].dt.strftime('%b %Y')
     
     level_drop, staff_drop = st.columns(2)
     level_select = level_drop.selectbox('Staff Level', ['All'] + [i for i in morale_df.LEVEL.sort_values().unique()])
