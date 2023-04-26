ffe51c91535b4274a0774d72408d5a86a5144268
Jeremy Shank <jpshank2@gmail.com>
2023-04-26 15:52:15 -0500

    writing staff and level select

--- reports/people.py
+++ reports/people.py
@@ -34,10 +34,17 @@ def level_4_people(st):
     boost_viz.plotly_chart(boost_fig, use_container_width=True)
     boost_tab.dataframe(boost_df[['EMPLOYEE', 'DATE', 'ENTHUSIASM', 'MEANING', 'PRIDE', 'CHALLENGE', 'ENERGY', 'STRONG', 'RECOVERY', 'ENDURANCE', 'AGG', 'TOTAL_HOURS', 'UTILIZATION']])
 
-    morale_df = get_rows("""select s.staff_name, s.level, m.*, enthusiasm + meaning + pride + challenge + energy + strong + recovery + endurance as agg
+    morale_df = get_rows("""select s.staff_name, s.level, m.*, enthusiasm + meaning + pride + challenge + energy + strong + recovery + endurance as agg, month(m.date) as month
         from people.morale m
             inner join dim_staff_master s on s.staffindex = m.staff
         where s.staff_status = 'Active' and
             s.level not in ('No Selection', 'Unknown');""")
+    
+    level_drop, staff_drop = st.columns(2)
+    level_select = level_drop.selectbox('Staff Level', ['All'] + [i for i in morale_df.LEVEL.sort_values().unique()])
+    if level_select == 'All':
+        staff_select = staff_drop.selectbox('Staff', ['All'] + [i for i in morale_df.STAFF_NAME.sort_values().unique()])
+    else:
+        staff_select = staff_drop.selectbox('Staff', ['All'] + [i for i in morale_df[morale_df['LEVEL'] == level_select].STAFF_NAME.sort_values().unique()])
     
     st.dataframe(morale_df)
