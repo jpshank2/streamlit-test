888b11437de36954a88925e7c67b2e8be10e177e
Jeremy Shank <jpshank2@gmail.com>
2023-04-26 14:11:54 -0500

    adding hover info

--- reports/people.py
+++ reports/people.py
@@ -18,7 +18,7 @@ def level_4_people(st):
         p.*
         from people.demo_boost p;""")
 
-    boost_fig = line(boost_df, x='DATE', y='AGG', color='EMPLOYEE', markers=True)
+    boost_fig = line(boost_df, x='DATE', y='AGG', color='EMPLOYEE', markers=True, hover_name='EMPLOYEE', hover_data=['DATE', 'AGG', 'ENTHUSIASM', 'MEANING', 'PRIDE', 'CHALLENGE', 'ENERGY', 'STRONG', 'RECOVERY', 'ENDURANCE'])
 
     boost_viz, boost_tab = st.tabs(['Visual', 'Table'])
     boost_viz.plotly_chart(boost_fig, use_container_width=True)
