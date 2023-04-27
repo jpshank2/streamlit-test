a38e0619c1273ec47e0e08578945ef0072d9ae79
Jeremy Shank <jpshank2@gmail.com>
2023-04-27 09:43:57 -0500

    setting iframe height to 900 and removing icon

--- pages/PowerBI Starter Pack.py
+++ pages/PowerBI Starter Pack.py
@@ -9,8 +9,8 @@ st.set_page_config(
     page_icon=Image.open(BytesIO(get('https://i.imgur.com/sL3m54K.png').content))
 )
 
-MainHeaderImage = Image.open(BytesIO(get('https://i.imgur.com/khnCmv8.png').content))
-one, two, three= st.columns(3)
-two.image(MainHeaderImage, use_column_width = True)
+# MainHeaderImage = Image.open(BytesIO(get('https://i.imgur.com/khnCmv8.png').content))
+# one, two, three= st.columns(3)
+# two.image(MainHeaderImage, use_column_width = True)
 
-st.write('<iframe src="https://api.bmss.com/cpa" width="1500" height="800"></iframe>', unsafe_allow_html=True)
+st.write('<iframe src="https://api.bmss.com/cpa" width="1500" height="900"></iframe>', unsafe_allow_html=True)
