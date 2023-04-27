00779a0f0de4ef6b14ea0e3bed34c03219470ac2
Jeremy Shank <jpshank2@gmail.com>
2023-04-27 09:35:52 -0500

    configuring power bi page

---
+++ pages/PowerBI Starter Pack.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+from requests import get
+from io import BytesIO
+
+st.set_page_config(
+    page_title='Power BI Starter Pack',
+    layout='wide',
+    page_icon=Image.open(BytesIO(get('https://i.imgur.com/sL3m54K.png').content))
+)
+
+MainHeaderImage = Image.open(BytesIO(get('https://i.imgur.com/khnCmv8.png').content))
+one, two, three= st.columns(3)
+two.image(MainHeaderImage, use_column_width = True)
+
+st.write('<iframe src="https://api.bmss.com/cpa"></iframe>', unsafe_allow_html=True)
--- pages/PowerBI Starter.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write('<iframe src="https://api.bmss.com/cpa"></iframe>', unsafe_allow_html=True)
